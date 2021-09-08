6422da47131d7552a56d9c124d6d6a7de1636a41
Joshua Patterson <jleegippies@gmail.com>
2021-09-07 21:46:35 -0500

    Added fire to ship

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -8,4 +8,9 @@ func _physics_process(delta):
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	)
 	
+	if direction.length() > 0.0:
+		$Fire.show()
+	else:
+		$Fire.hide()
+	
 	move_and_slide(direction * SPEED * delta)
