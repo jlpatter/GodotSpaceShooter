ed634eae85185b7e9234c408d5dcb70e30c6bf27
Joshua Patterson <jleegippies@gmail.com>
2021-09-30 14:22:53 -0500

    Fixed player movement and repositioned some things

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,16 +1,26 @@
 extends KinematicBody2D
 
-const SPEED = 5000.0
+const SPEED = 10000.0
+const ROTATION_SPEED = 2.0
+
+var rotation_dir = 0.0
 
 func _physics_process(delta):
-	var direction = Vector2(
-		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+	rotation_dir = 0.0
+	var velocity = Vector2(
+		0.0,
 		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
-	)
+	).rotated(rotation)
 	
-	if direction.length() > 0.0:
+	if Input.is_action_pressed("move_left"):
+		rotation_dir -= 1
+	elif Input.is_action_pressed("move_right"):
+		rotation_dir += 1
+	
+	if Input.get_action_strength("move_up"):
 		$Fire.show()
 	else:
 		$Fire.hide()
 	
-	move_and_slide(direction * SPEED * delta)
+	rotation += rotation_dir * ROTATION_SPEED * delta
+	move_and_slide(velocity * SPEED * delta)
