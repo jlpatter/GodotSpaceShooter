40dc4060c2bcc5af22a8e8250f8cffef0aaf1fd8
Joshua Patterson <jleegippies@gmail.com>
2021-09-30 15:57:04 -0500

    Set bullets to delete themselves after 10 seconds

--- src/Actors/GreenBullet.gd
+++ src/Actors/GreenBullet.gd
@@ -2,6 +2,12 @@ extends KinematicBody2D
 
 const SPEED = 10000.0
 
+var delete_time = 10.0
+
 func _physics_process(delta):
+	delete_time -= delta
+	if delete_time < 0.0:
+		queue_free()
+	
 	var dir = Vector2(0.0, -1.0).rotated(rotation)
 	move_and_slide(dir * SPEED * delta)
