3ab02bcd26cd6a6240db52a60cdccd4159efdd82
Joshua Patterson <jleegippies@gmail.com>
2021-10-05 18:57:40 -0500

    Doing some house-cleaning

--- src/Actors/Asteroid.gd
+++ src/Actors/Asteroid.gd
@@ -1,10 +1,18 @@
-extends KinematicBody2D
+extends Node2D
 
-const SPEED = 1000.0
+const SPEED = 10.0
 const ROTATION_SPEED = 2.0
 
 func _physics_process(delta):
 	var dir = Vector2(0.0, -1.0)
 	
 	rotation += ROTATION_SPEED * delta
-	move_and_slide(dir * SPEED * delta)
+	position += dir * SPEED * delta
+
+func _on_Area2D_body_entered(body):
+	if "GreenBullet" in body.name:
+		body.queue_free()
+		queue_free()
+	elif "Player" in body.name:
+		body.queue_free()
+		queue_free()
