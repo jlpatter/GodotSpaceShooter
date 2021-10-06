bc247480c420246aec00b86f646cd8ceee0e608a
Joshua Patterson <jleegippies@gmail.com>
2021-10-06 11:53:07 -0500

    WIP Added asteroid random start direction, working on SFX

--- src/Actors/Asteroid.gd
+++ src/Actors/Asteroid.gd
@@ -3,16 +3,24 @@ extends Node2D
 const SPEED = 10.0
 const ROTATION_SPEED = 2.0
 
+var direction
+
+func _ready():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	direction = Vector2(rng.randf() * 2 - 1, rng.randf() * 2 - 1).normalized()
+
 func _physics_process(delta):
-	var dir = Vector2(0.0, -1.0)
-	
 	rotation += ROTATION_SPEED * delta
-	position += dir * SPEED * delta
+	position += direction * SPEED * delta
+
+func explode():
+	$Explosion.play()
 
 func _on_Area2D_body_entered(body):
 	if "GreenBullet" in body.name:
 		body.queue_free()
-		queue_free()
+		explode()
 	elif "Player" in body.name:
 		body.queue_free()
-		queue_free()
+		explode()
