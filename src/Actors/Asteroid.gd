09342808b1900f8c04c2a0308bbc5915cdc8b580
Joshua Patterson <jleegippies@gmail.com>
2021-10-07 18:16:18 -0500

    Fixed bug with exploding asteroid damaging player

--- src/Actors/Asteroid.gd
+++ src/Actors/Asteroid.gd
@@ -4,6 +4,7 @@ const SPEED = 10.0
 const ROTATION_SPEED = 2.0
 
 var direction
+var is_exploding = false
 
 func _ready():
 	var rng = RandomNumberGenerator.new()
@@ -15,17 +16,18 @@ func _physics_process(delta):
 	position += direction * SPEED * delta
 
 func explode():
+	is_exploding = true
 	$Asteroid.hide()
 	$ExplosionAnim.show()
 	$ExplosionAnim.play()
 
 func _on_Area2D_body_entered(body):
-	if "Player" in body.name:
+	if "Player" in body.name and not is_exploding:
 		body.decrease_health(50)
 		explode()
 
 func _on_Area2D_area_entered(area):
-	if "GreenBullet" in area.get_parent().name:
+	if "GreenBullet" in area.get_parent().name and not is_exploding:
 		area.get_parent().queue_free()
 		explode()
 
