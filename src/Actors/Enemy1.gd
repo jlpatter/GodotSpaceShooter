fc6d78c2ae105cf235784797449e762cc2520084
Joshua Patterson <jleegippies@gmail.com>
2021-10-07 11:34:57 -0500

    Fixed enemy positioning and a null dereference

--- src/Actors/Enemy1.gd
+++ src/Actors/Enemy1.gd
@@ -5,6 +5,8 @@ const SPEED = 100.0
 var timer = 5.0
 
 func _physics_process(delta):
-	look_at(get_parent().get_node("Player").position)
+	var player_node = get_parent().get_node_or_null("Player")
+	if player_node != null:
+		look_at(player_node.position)
 	var dir = Vector2(1.0, 0.0).rotated(rotation)
 	position += dir * SPEED * delta
