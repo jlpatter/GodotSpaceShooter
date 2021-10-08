7277239b9b88731f202131ff86451bafa4ae0946
Joshua Patterson <jleegippies@gmail.com>
2021-10-07 19:56:35 -0500

    WIP Made a crude star map

---
+++ src/Levels/Map.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+
+func _on_EngageButton_pressed():
+	var pressed_destinations = []
+	for c in $Destinations.get_children():
+		if c.pressed:
+			pressed_destinations.append(c)
+	if pressed_destinations.size() != 1:
+		print("BLURG!")
+		# Display something about only selecting 1.
+	else:
+		# Blah blah do something about loading properties of the selected destination
+		get_tree().change_scene("res://src/Levels/Level1.tscn")
