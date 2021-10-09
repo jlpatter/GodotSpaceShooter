cb02e8cc30af022d0f520f3e13d64e50b1769b71
Joshua Patterson <jleegippies@gmail.com>
2021-10-08 21:16:55 -0500

    Added large moon

--- src/Levels/Level1.gd
+++ src/Levels/Level1.gd
@@ -2,8 +2,13 @@ extends Node2D
 
 onready var asteroid_prefab = preload("res://src/Actors/Asteroid.tscn")
 onready var enemy_prefab = preload("res://src/Actors/Enemy1.tscn")
+onready var moon_prefab = preload("res://Assets/LargeMoon.png")
 
 func _ready():
+	var random_parallax = GlobalVariables.rng.randi() % 2
+	if random_parallax == 1:
+		get_node("ParallaxBackground/ParallaxLayer2/LargeSun").texture = moon_prefab
+	
 	get_node("ParallaxBackground/ParallaxLayer2/LargeSun").position = Vector2(GlobalVariables.rng.randf() * 2000 - 1000, GlobalVariables.rng.randf() * 2000 - 1000)
 	
 	var asteroid_num = GlobalVariables.rng.randi() % 3 + 2
