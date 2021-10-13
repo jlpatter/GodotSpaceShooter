a8c9de769ee948c03c100cb0b6a59e8ed3be6872
Joshua Patterson <jleegippies@gmail.com>
2021-10-13 17:05:16 -0500

    Added a restart button

--- src/Misc/PauseMenu.gd
+++ src/Misc/PauseMenu.gd
@@ -13,3 +13,18 @@ func _on_QuitButton_pressed():
 
 func _on_SettingsButton_pressed():
 	$SettingsMenu.show()
+
+
+func _on_RestartButton_pressed():
+	MapVariables.is_first_load = true
+	MapVariables.locations = {}
+	MapVariables.current_color = ""
+	
+	PlayerVariables.health = 100
+	PlayerVariables.fuel = 0
+	PlayerVariables.has_green = false
+	PlayerVariables.has_red = false
+	PlayerVariables.has_blue = false
+	PlayerVariables.has_yellow = false
+	get_tree().paused = false
+	get_tree().change_scene("res://src/Levels/Level1.tscn")
