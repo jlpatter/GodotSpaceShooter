e326cc5d7f522ee45eb204ea278f09a7a4c3a356
Joshua Patterson <jleegippies@gmail.com>
2021-10-14 17:54:08 -0500

    WIP Drawn most of the boss, setting up green section currently

--- src/Levels/BossLevel/BlackObject.gd
+++ src/Levels/BossLevel/BlackObject.gd
@@ -1,6 +1,14 @@
 extends Node2D
 
-const SPEED = 10.0
+const SPEED = 100.0
+const ROTATION_SPEED = 2.0
+
+var timer = 15.0
 
 func _process(delta):
+	timer -= delta
+	rotation += ROTATION_SPEED * delta
 	position += Vector2(0.0, 1.0) * SPEED * delta
+	
+	if timer < 0.0:
+		queue_free()
--- src/Levels/BossLevel/BossLevel.gd
+++ src/Levels/BossLevel/BossLevel.gd
@@ -1,2 +1,28 @@
 extends Node2D
 
+var timer = 0.5
+
+onready var black_cube_prefab = preload("res://src/Levels/BossLevel/BlackCube.tscn")
+onready var black_pyramid_prefab = preload("res://src/Levels/BossLevel/BlackPyramid.tscn")
+onready var black_sphere_prefab = preload("res://src/Levels/BossLevel/BlackSphere.tscn")
+
+func _ready():
+	$BossGreenSection.position = Vector2(get_viewport().size.x * (5.0 / 6.0), get_node("BossGreenSection/BossGreenSection").texture.get_height())
+	$BossPlayer.position = Vector2(get_viewport().size.x / 2.0, get_viewport().size.y - get_node("BossPlayer/PlayerShip").texture.get_height())
+
+func _process(delta):
+	timer -= delta
+	if timer < 0.0:
+		var black_object = null
+		var object_picker = GlobalVariables.rng.randi() % 3
+		if object_picker == 0:
+			black_object = black_cube_prefab.instance()
+		elif object_picker == 1:
+			black_object = black_pyramid_prefab.instance()
+		elif object_picker == 2:
+			black_object = black_sphere_prefab.instance()
+		
+		add_child(black_object)
+		black_object.position = Vector2(GlobalVariables.rng.randf() * get_viewport().size.x, 0.0)
+		
+		timer = GlobalVariables.rng.randf() * 0.5
